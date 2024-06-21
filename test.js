6f719ef1146485d7445b868997b6d5f4b8ac0e2e
Jeremiah Johnson <jj.johnson@valpo.edu>
2024-06-21 01:43:48 -0500

    commit

--- test.js
+++ test.js
@@ -1,14 +1,21 @@
-function emailSend(){
 
-	
-	Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "gerejohns10@gmail.com",
-    Password : "1271D3A01200DF252D9C88F5B82EE48B4386Password",
-    To : 'gerejohns10@gmail.com',
-    From : "gerejohns10@gmail.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
+  function emailSend(){
+
+    var userName = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+  
+    var messageBody = "Name " + userName +
+    "<br/> Phone " + phone +
+    "<br/> Email " + email;
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "gerejohns10@gmail.com",
+      Password : "1271D3A01200DF252D9C88F5B82EE48B4386Password",
+      To : 'gerejohns10@gmail.com',
+      From : "gerejohns10@gmail.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
   }).then(
     message => {
       if(message=='OK'){
